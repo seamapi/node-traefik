5e8b181f2ac2ed6a74cddd854fa2817d46b60d0a
seveibar <seveibar@gmail.com>
2021-07-06 16:59:35 -0700

    more test improvements

--- tests/invalid-config.test.js
+++ tests/invalid-config.test.js
@@ -6,43 +6,17 @@ const micro = require("micro")
 const request = bent("string")
 
 test("invalid config should throw with relevant error", async (t) => {
-  const [webServicePort, traefikPort] = [await getPort(),await getPort()]
-
-  // Web service to proxy (on client)
-  const webService = micro((req, res) => "Hello world!")
-  webService.listen(webServicePort)
-
-  const traefikService = await traefik.start({
-    staticConfig: {
-      defaultEntryPoints: ["http"],
-      entryPoints:{
-        http: {
-          address: `pasta:pasta.com` // ERROR
-        }
-      },
-      // dot notation is also OK!
-      "log.level": "DEBUG"
-    },
-    dynamicConfig: {
+  await traefik.start({
+    defaultEntryPoints: ["http"],
+    entryPoints:{
       http: {
-        routers: {
-          someRouter: {
-            service: "someService",
-            rule: "PathPrefix(`/`)"
-          }
-        },
-        services: {
-          someService: {
-            "loadBalancer.servers": [{ url: `http://localhost:${webServicePort}` }]
-          }
-        }
+        address: `pasta:pasta.com` // ERROR
       }
-    }
+    },
+    "log.level": "DEBUG"
+  }).then(() => { 
+    t.fail("should throw error")
+  }).catch((err) => {
+    t.assert(err.toString().includes("pasta.com: unknown port"))
   })
-
-  const response = await request(`http://localhost:${traefikPort}`)
-  t.assert(response === "Hello world!")
-
-  await traefikService.stop()
-  await webService.close()
 })
